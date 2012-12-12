d5e8b134918ef17e4502c2dbf2b2b88de4b6b432
Rolf Bislin <romibi2@gmail.com>
2012-12-12 18:22:55 +0100

    Added AbstractKeywordEvent and EnableCaveMapping Parsers

---
+++ src/ch/romibi/minecraft/toIrc/parsers/AbstractKeywordEvent.java
@@ -0,0 +1,18 @@
+package ch.romibi.minecraft.toIrc.parsers;
+
+import jerklib.events.MessageEvent;
+import ch.romibi.minecraft.toIrc.interfaces.MessageParser;
+
+public abstract class AbstractKeywordEvent implements MessageParser {
+	protected String keyword;
+	
+	@Override
+	public void parse(MessageEvent me) {
+		if(me.getMessage().toLowerCase().equals("."+keyword)) {
+			doEvent(me);
+		}
+	}
+	
+	protected abstract void doEvent(MessageEvent me);
+
+}
---
+++ src/ch/romibi/minecraft/toIrc/parsers/EnableCaveMapping.java
@@ -0,0 +1,19 @@
+package ch.romibi.minecraft.toIrc.parsers;
+
+import ch.romibi.minecraft.toIrc.McToIrc;
+import jerklib.events.MessageEvent;
+
+public class EnableCaveMapping extends AbstractKeywordEvent {
+	
+	protected String keyword = "enCave";
+	
+	@Override
+	protected void doEvent(MessageEvent me) {
+		if(McToIrc.irc.convertUsernameToMc(me.getNick()) == null) {
+			me.getSession().sayPrivate(me.getNick(), "Sorry! This command is only aviable for Minecraft-Users!");
+		} else {
+			McToIrc.sendCommandToMc("tell "+McToIrc.irc.convertUsernameToMc(me.getNick())+" not yet implemented");
+		}
+	}
+
+}
